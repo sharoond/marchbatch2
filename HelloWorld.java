364d84249f05cc4035b189e40309ea1554afec05
jatinbhalla <jatinbhalla18.jb@gmail.com>
2026-03-09 15:22:26 +0530

    java code changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Welcome to Java GitHub Actions!");
+        System.out.println("Welcome to jatin!");
         
         // Display system information
         System.out.println("Java Version: " + System.getProperty("java.version"));
